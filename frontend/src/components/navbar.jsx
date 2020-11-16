1e1dad53cf7c13f2b4a01315e555ba7b3c0de138
jakechanan <jake_chanan@brown.edu>
2020-11-16 17:01:06 -0600

    backend basic setup

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+export default class Navbar extends React.Component {
+  render() {
+    return (<div>[Navbar component]
+    </div>)
+  }
+}
---
+++ frontend/src/components/sidebar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+export default class Sidebar extends React.Component {
+  render() {
+    return (<div>[Sidebar component]
+    </div>)
+  }
+}
