6ea398995865cdd005697b773ec3cab5af596a99
jakechanan <jake_chanan@brown.edu>
2020-11-20 08:14:20 -0600

    trying to fix npm for heroku

--- backend/util.js
+++ backend/util.js
@@ -1,14 +1,10 @@
 import auth0 from 'auth0';
-import fs from 'fs';
 
 export function createManagementClient() {
-  const secret = fs.readFileSync('./.secret').toString().trim()
-  console.log(secret)
-
   return new auth0.ManagementClient({
     domain: 'dev-o0dlw7sn.us.auth0.com',
     clientId: 'gFN0a3JsIUUu9OW6X5Qpsd0uzG6b4kio',
-    clientSecret: secret,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
     scope: 'read:users',
     audience: 'https://dev-o0dlw7sn.us.auth0.com/api/v2/',
     tokenProvider: {
