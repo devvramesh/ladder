8ae4ecd5b32807b7096d07523eadba218745a464
Kirache <49444055+Kirache@users.noreply.github.com>
2020-11-20 02:15:05 -0800

    homepage with button links

--- frontend/src/components/homepage.jsx
+++ frontend/src/components/homepage.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import {makeBackendRequest} from "../util"
-import Navbar from "./navbar";
+import LoginButton from "./login_button";
 
 export default class Home extends React.Component {
-  render() {
-    return (<div>
-      <h1>Homepage! Welcome!</h1>
-      <Navbar></Navbar>
-      <div>Note: the navbar is just here for testing, feel free to remove it.</div>
-    </div>)
-  }
+    render() {
+        return (<div>
+            <Link to="/login">
+                <button>Login</button>
+            </Link>
+            <Link to="/signup">
+                <button>Sign Up</button>
+            </Link>
+            <h1>Ladder</h1>
+            <Link to="/search?category=employee">
+                <button>Find Employees</button>
+            </Link>
+            <Link to="/search?category=job">
+                <button>See Who's Hiring</button>
+            </Link>
+        </div>)
+    }
 }
