b17f28b3d21186f846bafe6abb5d23ac17020133
Kirache <49444055+Kirache@users.noreply.github.com>
2020-11-20 02:16:58 -0800

    homepage with button links
    last commit was named strangely on its own for some reason

--- frontend/src/components/homepage.jsx
+++ frontend/src/components/homepage.jsx
@@ -5,20 +5,22 @@ import LoginButton from "./login_button";
 
 export default class Home extends React.Component {
     render() {
-        return (<div>
-            <Link to="/login">
-                <button>Login</button>
-            </Link>
-            <Link to="/signup">
-                <button>Sign Up</button>
-            </Link>
-            <h1>Ladder</h1>
-            <Link to="/search?category=employee">
-                <button>Find Employees</button>
-            </Link>
-            <Link to="/search?category=job">
-                <button>See Who's Hiring</button>
-            </Link>
-        </div>)
+        return (
+            <div>
+                <Link to="/login">
+                    <button>Login</button>
+                </Link>
+                <Link to="/signup">
+                    <button>Sign Up</button>
+                </Link>
+                <h1>Ladder</h1>
+                <Link to="/search?category=employee">
+                    <button>Find Employees</button>
+                </Link>
+                <Link to="/search?category=job">
+                    <button>See Who's Hiring</button>
+                </Link>
+            </div>
+        )
     }
 }
