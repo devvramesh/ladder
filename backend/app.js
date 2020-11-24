737c003ff6961d9a31ebe35eb7c544ceff92abea
jakechanan <jake_chanan@brown.edu>
2020-11-24 09:42:11 -0600

    db init -- using heroku postgres

--- backend/app.js
+++ backend/app.js
@@ -7,120 +7,129 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import fs from 'fs';
 import { createManagementClient } from './util.js'
+import { dbInit, withDB, logDBInfo } from './db.js'
 
 const PORT = process.env.PORT || 5000
+const DEBUG = true;
 
 const app = express();
 const jsonParser = bodyParser.json();
-
 app.use(cors());
-
 const managementClient = createManagementClient();
 
-// NOTE(jake): set this to true to print out request bodies when they arrive
-const DEBUG = true;
+function serveApp() {
+  console.log('Serving backend...')
 
-console.log('Serving backend...')
-
-const post = (route, callback) => {
-  return app.post(route, jsonParser, (req, res) => {
-    if (DEBUG) {
+  const post = (route, callback) => {
+    return app.post(route, jsonParser, (req, res) => {
+      if (DEBUG) {
+        console.log('---------------------------------------------');
+        console.log(`DEBUG: [route: ${route}]`);
+        console.log("Request body:");
+        console.log(req.body);
+      }
+      const result = callback(req, res);
       console.log('---------------------------------------------');
-      console.log(`DEBUG: [route: ${route}]`);
-      console.log("Request body:");
-      console.log(req.body);
-    }
-    const result = callback(req, res);
-    console.log('---------------------------------------------');
-    return result;
+      return result;
+    });
+  };
+
+  post('/api/login', (req, res) => {
+    // TODO: implement login!!
   });
-};
 
-post('/api/login', (req, res) => {
-  // TODO: implement login!!
-});
+  post('/api/signup', (req, res) => {
+    // TODO: implement signup!!
+  });
 
-post('/api/signup', (req, res) => {
-  // TODO: implement signup!!
-});
-
-post('/api/search', (req, res) => {
-  // TODO: implement search!!
-  const dummy_results = [
-    {name: "Alice", location: "Kingston", job: "Electrician"},
-    {name: "Bob", location: "Providence", job: "Roofer"},
-    {name: "Charlie", location: "Providence", job: "Construction Worker"}
-  ]
-
-  res.send(JSON.stringify({searchResults: dummy_results}));
-});
-
-post('/api/favorites', (req, res) => {
-  // TODO: implement search!!
-  const category = req.body.category;
-  let dummy_results = [];
-  if (category === "employee") {
-    dummy_results = [
+  post('/api/search', (req, res) => {
+    // TODO: implement search!!
+    const dummy_results = [
       {name: "Alice", location: "Kingston", job: "Electrician"},
       {name: "Bob", location: "Providence", job: "Roofer"},
       {name: "Charlie", location: "Providence", job: "Construction Worker"}
     ]
-  } else if (category === "job") {
-    dummy_results = [
-      {name: "Company A", location: "Kingston", job: "Electrician"},
-      {name: "Company B", location: "Providence", job: "Roofer"},
-      {name: "Company C", location: "Providence", job: "Construction Worker"}
-    ]
-  } else if (category === "company") {
-    dummy_results = [
-      {name: "Company A", location: "Kingston"},
-      {name: "Company B", location: "Providence"},
-      {name: "Company C", location: "Providence"}
-    ]
-  }
 
-  res.send(JSON.stringify({favorites: dummy_results}));
-});
-
-post('/api/user_info', (req, res) => {
-  const userID = req.body.userID;
-  let result = null;
-  if (!userID) {
-    res.send(JSON.stringify({accountType: null}));
-    return;
-  }
-  managementClient.getUser({ id: userID }, function (err, user) {
-    if (user) {
-      res.send(JSON.stringify({
-        username: user.username,
-        accountType: user.user_metadata.accountType
-      }));
-    } else {
-      res.send(JSON.stringify({accountType: null}));
-    }
+    res.send(JSON.stringify({searchResults: dummy_results}));
   });
-});
 
-console.log('Backend served.')
+  post('/api/favorites', (req, res) => {
+    // TODO: implement search!!
+    const category = req.body.category;
+    let dummy_results = [];
+    if (category === "employee") {
+      dummy_results = [
+        {name: "Alice", location: "Kingston", job: "Electrician"},
+        {name: "Bob", location: "Providence", job: "Roofer"},
+        {name: "Charlie", location: "Providence", job: "Construction Worker"}
+      ]
+    } else if (category === "job") {
+      dummy_results = [
+        {name: "Company A", location: "Kingston", job: "Electrician"},
+        {name: "Company B", location: "Providence", job: "Roofer"},
+        {name: "Company C", location: "Providence", job: "Construction Worker"}
+      ]
+    } else if (category === "company") {
+      dummy_results = [
+        {name: "Company A", location: "Kingston"},
+        {name: "Company B", location: "Providence"},
+        {name: "Company C", location: "Providence"}
+      ]
+    }
 
-if (process.env.RUN_MODE === "HEROKU") {
-  console.log("Running on Heroku. Frontend should have been built already.")
-  console.log("Serving frontend...")
+    res.send(JSON.stringify({favorites: dummy_results}));
+  });
 
-  // Serve frontend
-  app.use(express.static('/app/frontend/build'))
+  post('/api/user_info', (req, res) => {
+    const userID = req.body.userID;
+    let result = null;
+    if (!userID) {
+      res.send(JSON.stringify({accountType: null}));
+      return;
+    }
+    managementClient.getUser({ id: userID }, function (err, user) {
+      if (user) {
+        res.send(JSON.stringify({
+          username: user.username,
+          accountType: user.user_metadata.accountType
+        }));
+      } else {
+        res.send(JSON.stringify({accountType: null}));
+      }
+    });
+  });
 
-  // Necessary for paths with query string
-  app.get('*', (req, res) => {
-    res.sendFile('/app/frontend/build/index.html')
-  })
+  console.log('Backend served.')
 
-  console.log('Frontend served.')
-} else {
-  console.log("Running locally. Start frontend with [cd frontend; npm start]")
+  if (process.env.RUN_MODE === "HEROKU") {
+    console.log("Running on Heroku. Frontend should have been built already.")
+    console.log("Serving frontend...")
+
+    // Serve frontend
+    app.use(express.static('/app/frontend/build'))
+
+    // Necessary for paths with query string
+    app.get('*', (req, res) => {
+      res.sendFile('/app/frontend/build/index.html')
+    })
+
+    console.log('Frontend served.')
+  } else {
+    console.log("Running locally. Start frontend with [cd frontend; npm start]")
+  }
+
+  app.listen(PORT, () => {
+    console.log(`Backend server listening on port ${PORT}`);
+    console.log('---------------------------------------------');
+  });
 }
 
-app.listen(PORT, () => {
-  console.log(`Backend server listening on port ${PORT}`);
-  console.log('---------------------------------------------');
-});
+async function main() {
+  console.log("Initializing DB...")
+  await dbInit();
+  await logDBInfo();
+  console.log("Done initializing DB.")
+  serveApp();
+}
+
+main()
--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,16 @@
-const sqlite3 = require('sqlite3');
-const path = require('path');
-const mkdirp = require('mkdirp');
-const fs = require('fs');
-const util = require('./util')
+import dotenv from 'dotenv';
+dotenv.config()
 
-const DB_DIRPATH = "./db"
-mkdirp(DB_DIRPATH)
-const DB_NAME = "ladder.sqlite3"
-const DB_PATH = path.join(DB_DIRPATH, DB_NAME);
+import * as pg from 'pg'
+const { Client, Pool } = pg.default;
+
+import fixedWidthString from 'fixed-width-string';
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+})
+
 
 let initialized = false;
 
@@ -15,91 +18,163 @@ let initialized = false;
 // and fills it with dummy data.
 let TESTING = true;
 
-withDB = (fn) => {
-  let conn;
-  let result;
+export async function withDB(fn) {
+  const client = await pool.connect()
+  let result = null;
   try {
-    conn = new sqlite3.Database(DB_PATH);
-    conn.serialize(() => {
-      if (!initialized) {
-        conn.serialize(() => dbInit(conn));
-        initialized = true;
-      }
-      result = conn.serialize(() => fn(conn));
-    })
+    await client.query('BEGIN')
+    result = await fn(client)
+    await client.query('COMMIT')
+  } catch (e) {
+    await client.query('ROLLBACK')
+    throw e
   } finally {
-    conn.close();
+    client.release()
   }
   return result;
 }
 
-function dbInit(db) {
-  console.log('initializing DB')
-  db.serialize(() => {
-    db.run(
+export async function dbInit() {
+  await withDB(async (client) => {
+    await client.query(
       `CREATE TABLE IF NOT EXISTS users(
-        id TEXT NOT NULL PRIMARY KEY,
-        type TEXT NOT NULL CHECK(type = 'employee' OR type =='employer'),
+        auth0_user_id TEXT NOT NULL PRIMARY KEY,
+        username TEXT NOT NULL UNIQUE,
+        type TEXT NOT NULL CHECK(type = 'employee' OR type = 'employer'),
         name TEXT NOT NULL,
         email TEXT NOT NULL UNIQUE,
         phone TEXT UNIQUE,
         location TEXT,
-        profile_img BLOB
+        profile_img_url TEXT
       )`
     )
 
-    // db.run(
-    //   `CREATE TABLE IF NOT EXISTS employees(
-    //     id TEXT NOT NULL PRIMARY KEY,
-    //     qualifications TEXT,
-    //     about TEXT,
-    //     looking_for TEXT,
-    //     )
-    //
-    //   FOREIGN KEY (id) REFERENCES users(id)`
-    //   );
-  });
-  console.log('done initializing DB')
-}
+    await client.query(
+      `CREATE TABLE IF NOT EXISTS employees(
+        auth0_user_id TEXT NOT NULL PRIMARY KEY,
+        qualifications TEXT,
+        about TEXT,
+        looking_for TEXT,
 
-if (TESTING) {
-  // reset DB
-  fs.unlinkSync(DB_PATH);
-
-  // insert dummy data
-  withDB((conn) => {
-    conn.run(
-      `INSERT INTO users(id, type, name, email)
-        VALUES (
-          '1',
-          'employee',
-          'Alice A.',
-          'alice@aol.com'
-        )`
+        FOREIGN KEY (auth0_user_id) REFERENCES users(auth0_user_id)
+      )`
     )
 
-    conn.run(
-      `INSERT INTO users(id, type, name, email)
-        VALUES (
-          '2',
-          'employee',
-          'Bob B.',
-          'bob@aol.com'
-        )`
+    await client.query(
+      `CREATE TABLE IF NOT EXISTS employers(
+        auth0_user_id TEXT NOT NULL PRIMARY KEY,
+        about TEXT,
+        logistics TEXT,
+
+        FOREIGN KEY (auth0_user_id) REFERENCES users(auth0_user_id)
+      )`
     )
 
-    conn.run(
-      `INSERT INTO users(id, type, name, email)
-        VALUES (
-          '3',
-          'employee',
-          'Charlie C.',
-          'charlie@aol.com'
-        )`
-    )
+    await client.query(
+      `CREATE TABLE IF NOT EXISTS jobs(
+        post_id SERIAL NOT NULL PRIMARY KEY,
+        employer_auth0_user_id TEXT NOT NULL UNIQUE,
+        job_title TEXT NOT NULL,
+        description TEXT,
+        qualifications TEXT,
+        logistics TEXT,
+        job_image_url TEXT,
 
-    conn.each("SELECT * from users", function(err, row) {
-      console.log(row);
-  });
+        FOREIGN KEY (employer_auth0_user_id) REFERENCES employers(auth0_user_id)
+      )`
+    )
   })
 }
+
+export async function logDBInfo() {
+  await withDB(async (client) => {
+    const schema = await client.query(`
+      SELECT table_name, string_agg(column_name, ', ' ORDER BY column_name) AS columns
+      FROM
+        information_schema.tables AS t
+        NATURAL JOIN information_schema.columns AS c
+      WHERE table_schema = 'public'
+      GROUP BY table_name
+      ORDER BY table_name;
+    `)
+
+    console.log("----------------------------------------------------------------------------------------------------")
+    console.log("DB SCHEMA:")
+    console.log("----------------------------------------------------------------------------------------------------")
+    schema.rows.forEach((row) => {
+      console.log(`${fixedWidthString(row.table_name, 9)} | ${row.columns}`)
+    })
+    console.log("----------------------------------------------------------------------------------------------------")
+  })
+}
+
+
+//   console.log('initializing DB')
+//   db.serialize(() => {
+//     db.run(
+//       `CREATE TABLE IF NOT EXISTS users(
+//         id TEXT NOT NULL PRIMARY KEY,
+//         type TEXT NOT NULL CHECK(type = 'employee' OR type =='employer'),
+//         name TEXT NOT NULL,
+//         email TEXT NOT NULL UNIQUE,
+//         phone TEXT UNIQUE,
+//         location TEXT,
+//         profile_img BLOB
+//       )`
+//     )
+//
+//     // db.run(
+//     //   `CREATE TABLE IF NOT EXISTS employees(
+//     //     id TEXT NOT NULL PRIMARY KEY,
+//     //     qualifications TEXT,
+//     //     about TEXT,
+//     //     looking_for TEXT,
+//     //     )
+//     //
+//     //   FOREIGN KEY (id) REFERENCES users(id)`
+//     //   );
+//   });
+//   console.log('done initializing DB')
+// }
+//
+// if (TESTING) {
+//   // reset DB
+//   fs.unlinkSync(DB_PATH);
+//
+//   // insert dummy data
+//   withDB((conn) => {
+//     conn.run(
+//       `INSERT INTO users(id, type, name, email)
+//         VALUES (
+//           '1',
+//           'employee',
+//           'Alice A.',
+//           'alice@aol.com'
+//         )`
+//     )
+//
+//     conn.run(
+//       `INSERT INTO users(id, type, name, email)
+//         VALUES (
+//           '2',
+//           'employee',
+//           'Bob B.',
+//           'bob@aol.com'
+//         )`
+//     )
+//
+//     conn.run(
+//       `INSERT INTO users(id, type, name, email)
+//         VALUES (
+//           '3',
+//           'employee',
+//           'Charlie C.',
+//           'charlie@aol.com'
+//         )`
+//     )
+//
+//     conn.each("SELECT * from users", function(err, row) {
+//       console.log(row);
+//     });
+//   })
+// }
