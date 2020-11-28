be543c5b5ca385f7a0ab6f988a02afca2a903ed3
Kirache <49444055+Kirache@users.noreply.github.com>
2020-11-27 19:18:43 -0800

    Some css for home
    Still working

--- frontend/src/components/homepage.jsx
+++ frontend/src/components/homepage.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import {Link} from "react-router-dom";
 import {makeBackendRequest} from "../util"
 import LoginButton from "./login_button";
+import './home.css'
 
 export default class Home extends React.Component {
     render() {
         return (
             <div>
                 <LoginButton redirectUri={window.location.origin}></LoginButton>
-                <h1>Ladder</h1>
+                <h1 class="logo">Ladder</h1>
                 <Link to="/search?category=employee">
-                    <button>Find Employees</button>
+                    <button class="large-button">Find Employees</button>
                 </Link>
                 <Link to="/search?category=job">
-                    <button>See Who's Hiring</button>
+                    <button class="large-button">See Who's Hiring</button>
                 </Link>
             </div>
         )
